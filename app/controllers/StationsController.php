e41c2c1adaf398dafd92eb29c0bb463f34064a39
tstimur <tstimurb0311@gmail.com>
2024-07-10 02:38:57 +0700

    Refactor: Added response status. Refactored functions actionAddStation, actionUpdateStation, actionDeleteStation.

--- app/controllers/StationsController.php
+++ app/controllers/StationsController.php
@@ -54,96 +54,29 @@ class StationsController extends ActiveController
         try {
             $station = new Stations();
             if ($station->load($data, '') && $station->validate() && $station->save()) {
-                if (!empty($data['stationsTranslations'])) {
-                    $translation = new StationsTranslation();
-                    $translation->station_id = $station->id;
-                    $translation->language_id = $data['stationsTranslations']['language_id'];
-                    $translation->value = $station->name;
-                    if ($translation->load($data['stationsTranslations'], '') && $translation->validate()) {
-                        if (!$translation->save()) {
-                            throw new Exception('Failed to save station translation');
-                        }
-                    } else {
-                        throw new Exception('Station translation validation failed');
-                    }
-                }
+                $this->addTranslation($data, $station);
+                $this->addTransfer($data, $station);
+                $this->addAudio($data, $station);
+                $this->addFeature($data, $station);
+                $this->addExit($data, $station);
 
-                if (!empty($data['stationsTransfers'])) {
-
-                    $transfer = new StationsTransfers();
-                    $transfer->station_id = $station->id;
-                    $transfer->station_to_id = $station->id;
-                    $transfer->type = $data['stationsTransfers']['type'];
-                    $transfer->name = $data['stationsTransfers']['name'];
-                    $transfer->code = $data['stationsTransfers']['code'];
-                    $transfer->icon = $data['stationsTransfers']['icon'] ?? '';
-                    if ($transfer->load($data['stationsTransfers'], '') && $transfer->validate()) {
-                        if (!$transfer->save()) {
-                            throw new Exception('Failed to save station transfer');
-                        }
-                    } else {
-                        throw new Exception('Station transfer validation failed');
-                    }
-                }
-
-                if (!empty($data['stationsAudio'])) {
-
-                    $audio = new StationsAudio();
-                    $audio->station_id = $station->id;
-                    $audio->direction = $data['stationsAudio']['direction'];
-                    $audio->action = $data['stationsAudio']['action'];
-                    $audio->sound = $data['stationsAudio']['sound'];
-                    if ($audio->load($data['stationsAudio'], '') && $audio->validate()) {
-                        if (!$audio->save()) {
-                            throw new Exception('Failed to save station audio');
-                        }
-                    } else {
-                        throw new Exception('Station audio validation failed');
-                    }
-                }
-
-                if (!empty($data['stationsFeatures'])) {
-                    $feature = new StationsFeatures();
-                    $feature->station_id = $station->id;
-                    $feature->feature_id = $data['stationsFeatures']['feature_id'];
-                    if ($feature->load($data['stationsFeatures'], '') && $feature->validate()) {
-                        if (!$feature->save()) {
-                            throw new Exception('Failed to save station feature');
-                        }
-                    } else {
-                        throw new Exception('Station feature validation failed');
-                    }
-                }
-
-                if (!empty($data['stationsExits'])) {
-
-                    $exit = new stationsExits();
-                    $exit->station_id = $station->id;
-                    $exit->direction = $data['stationsExits']['direction'];
-                    $exit->doors = $data['stationsExits']['doors'];
-                    if ($exit->load($data['stationsExits'], '') && $exit->validate()) {
-                        if (!$exit->save()) {
-                            throw new Exception('Failed to save station exit');
-                        }
-                    } else {
-                        throw new Exception('Station exit validation failed');
-                    }
-                }
                 $transaction->commit();
+                Yii::$app->response->statusCode = 201;
                 return [
                     'status' => 'success',
                     'message' => 'Station and related data created successfully',
                 ];
             } else {
+                Yii::$app->response->statusCode = 400;
                 return [
                     'status' => 'error',
                     'message' => 'Failed to create station',
                     'errors' => $station->errors,
                 ];
             }
-
         } catch (Exception $exception) {
             $transaction->rollBack();
+            Yii::$app->response->statusCode = 500;
             return [
                 'status' => 'error',
                 'message' => 'An error occurred while saving the station',
@@ -171,85 +104,21 @@ class StationsController extends ActiveController
 
         try {
             if ($station->load($data, '') && $station->validate() && $station->save()) {
-                if (!empty($data['stationsTranslations'])) {
-                    $translation = new StationsTranslation();
-                    $translation->station_id = $station->id;
-                    $translation->language_id = $data['stationsTranslations']['language_id'];
-                    $translation->value = $station->name;
-                    if ($translation->load($data['stationsTranslations'], '') && $translation->validate()) {
-                        if (!$translation->save()) {
-                            throw new Exception('Failed to save station translation');
-                        }
-                    } else {
-                        throw new Exception('Station translation validation failed');
-                    }
-                }
-
+                $this->addTranslation($data, $station);
                 StationsTransfers::deleteAll(['station_id' => $id]);
-                if (!empty($data['stationsTransfers'])) {
-                    $transfer = new StationsTransfers();
-                    $transfer->station_id = $station->id;
-                    $transfer->station_to_id = $station->id;
-                    $transfer->type = $data['stationsTransfers']['type'];
-                    $transfer->name = $data['stationsTransfers']['name'];
-                    $transfer->code = $data['stationsTransfers']['code'];
-                    $transfer->icon = $data['stationsTransfers']['icon'] ?? '';
-                    if ($transfer->load($data['stationsTransfers'], '') && $transfer->validate()) {
-                        if (!$transfer->save()) {
-                            throw new Exception('Failed to save station transfer');
-                        }
-                    } else {
-                        throw new Exception('Station transfer validation failed');
-                    }
-                }
+                $this->addTransfer($data, $station);
+                $this->addAudio($data, $station);
+                $this->addFeature($data, $station);
+                $this->addExit($data, $station);
 
-                if (!empty($data['stationsAudio'])) {
-                    $audio = new StationsAudio();
-                    $audio->station_id = $station->id;
-                    $audio->direction = $data['stationsAudio']['direction'];
-                    $audio->action = $data['stationsAudio']['action'];
-                    $audio->sound = $data['stationsAudio']['sound'];
-                    if ($audio->load($data['stationsAudio'], '') && $audio->validate()) {
-                        if (!$audio->save()) {
-                            throw new Exception('Failed to save station audio');
-                        }
-                    } else {
-                        throw new Exception('Station audio validation failed');
-                    }
-                }
-
-                if (!empty($data['stationsFeatures'])) {
-                    $feature = new StationsFeatures();
-                    $feature->station_id = $station->id;
-                    $feature->feature_id = $data['stationsFeatures']['feature_id'];
-                    if ($feature->load($data['stationsFeatures'], '') && $feature->validate()) {
-                        if (!$feature->save()) {
-                            throw new Exception('Failed to save station feature');
-                        }
-                    } else {
-                        throw new Exception('Station feature validation failed');
-                    }
-                }
-
-                if (!empty($data['stationsExits'])) {
-                    $exit = new stationsExits();
-                    $exit->station_id = $station->id;
-                    $exit->direction = $data['stationsExits']['direction'];
-                    $exit->doors = $data['stationsExits']['doors'];
-                    if ($exit->load($data['stationsExits'], '') && $exit->validate()) {
-                        if (!$exit->save()) {
-                            throw new Exception('Failed to save station exit');
-                        }
-                    } else {
-                        throw new Exception('Station exit validation failed');
-                    }
-                }
                 $transaction->commit();
+                Yii::$app->response->statusCode = 201;
                 return [
                     'status' => 'success',
                     'message' => 'Station and related data updated successfully',
                 ];
             } else {
+                Yii::$app->response->statusCode = 400;
                 return [
                     'status' => 'error',
                     'message' => 'Failed to update station',
@@ -258,6 +127,7 @@ class StationsController extends ActiveController
             }
         } catch (Exception $exception) {
             $transaction->rollBack();
+            Yii::$app->response->statusCode = 500;
             return [
                 'status' => 'error',
                 'message' => 'An error occurred while updating the station',
@@ -265,6 +135,7 @@ class StationsController extends ActiveController
             ];
         }
     }
+
 
     /**
      * @param int $id
@@ -292,7 +163,8 @@ class StationsController extends ActiveController
                 Yii::$app->response->statusCode = 400;
                 return [
                     'status' => 'error',
-                    'message' => 'Failed to delete station'
+                    'message' => 'Failed to delete station',
+                    'errors' => $station->errors,
                 ];
             }
             $transaction->commit();
@@ -310,6 +182,176 @@ class StationsController extends ActiveController
                 'message' => 'An error occurred while deleting the station',
                 'errors' => $exception->getMessage(),
             ];
+        }
+    }
+
+    /**
+     * @param $data
+     * @param Stations $station
+     * @return array|void
+     * @throws \yii\db\Exception
+     */
+    public function addTranslation($data, Stations $station)
+    {
+        if (!empty($data['stationsTranslations'])) {
+            $translation = new StationsTranslation();
+            $translation->station_id = $station->id;
+            $translation->language_id = $data['stationsTranslations']['language_id'];
+            $translation->value = $station->name;
+            if ($translation->load($data['stationsTranslations'], '') && $translation->validate()) {
+                if (!$translation->save()) {
+                    Yii::$app->response->statusCode = 400;
+                    return [
+                        'status' => 'error',
+                        'message' => 'Failed to save station translation',
+                        'errors' => $translation->errors,
+                    ];
+                }
+            } else {
+                Yii::$app->response->statusCode = 400;
+                return [
+                    'status' => 'error',
+                    'message' => 'Failed to verify the transfer of the station',
+                    'errors' => $translation->errors,
+                ];
+            }
+        }
+    }
+
+
+    /**
+     * @param $data
+     * @param Stations $station
+     * @return array|void
+     * @throws \yii\db\Exception
+     */
+    public function addTransfer($data, Stations $station)
+    {
+        if (!empty($data['stationsTransfers'])) {
+            $transfer = new StationsTransfers();
+            $transfer->station_id = $station->id;
+            $transfer->station_to_id = $station->id;
+            $transfer->type = $data['stationsTransfers']['type'];
+            $transfer->name = $data['stationsTransfers']['name'];
+            $transfer->code = $data['stationsTransfers']['code'];
+            $transfer->icon = $data['stationsTransfers']['icon'] ?? '';
+            if ($transfer->load($data['stationsTransfers'], '') && $transfer->validate()) {
+                if (!$transfer->save()) {
+                    Yii::$app->response->statusCode = 400;
+                    return [
+                        'status' => 'error',
+                        'message' => 'Failed to save station transfer',
+                        'errors' => $transfer->errors,
+                    ];
+                }
+            } else {
+                Yii::$app->response->statusCode = 400;
+                return [
+                    'status' => 'error',
+                    'message' => 'Station transfer validation failed',
+                    'errors' => $transfer->errors,
+                ];
+            }
+        }
+    }
+
+    /**
+     * @param $data
+     * @param Stations $station
+     * @return array|void
+     * @throws \yii\db\Exception
+     */
+    public function addAudio($data, Stations $station)
+    {
+        if (!empty($data['stationsAudio'])) {
+
+            $audio = new StationsAudio();
+            $audio->station_id = $station->id;
+            $audio->direction = $data['stationsAudio']['direction'];
+            $audio->action = $data['stationsAudio']['action'];
+            $audio->sound = $data['stationsAudio']['sound'];
+            if ($audio->load($data['stationsAudio'], '') && $audio->validate()) {
+                if (!$audio->save()) {
+                    Yii::$app->response->statusCode = 400;
+                    return [
+                        'status' => 'error',
+                        'message' => 'Failed to save station audio',
+                        'errors' => $audio->errors,
+                    ];
+                }
+            } else {
+                Yii::$app->response->statusCode = 400;
+                return [
+                    'status' => 'error',
+                    'message' => 'Station audio validation failed',
+                    'errors' => $audio->errors,
+                ];
+            }
+        }
+    }
+
+    /**
+     * @param $data
+     * @param Stations $station
+     * @return array|void
+     * @throws \yii\db\Exception
+     */
+    public function addFeature($data, Stations $station)
+    {
+        if (!empty($data['stationsFeatures'])) {
+            $feature = new StationsFeatures();
+            $feature->station_id = $station->id;
+            $feature->feature_id = $data['stationsFeatures']['feature_id'];
+            if ($feature->load($data['stationsFeatures'], '') && $feature->validate()) {
+                if (!$feature->save()) {
+                    Yii::$app->response->statusCode = 400;
+                    return [
+                        'status' => 'error',
+                        'message' => 'Failed to save station feature',
+                        'errors' => $feature->errors,
+                    ];
+                }
+            } else {
+                Yii::$app->response->statusCode = 400;
+                return [
+                    'status' => 'error',
+                    'message' => 'Station feature validation failed',
+                    'errors' => $feature->errors,
+                ];
+            }
+        }
+    }
+
+    /**
+     * @param $data
+     * @param Stations $station
+     * @return array|void
+     * @throws \yii\db\Exception
+     */
+    public function addExit($data, Stations $station)
+    {
+        if (!empty($data['stationsExits'])) {
+            $exit = new stationsExits();
+            $exit->station_id = $station->id;
+            $exit->direction = $data['stationsExits']['direction'];
+            $exit->doors = $data['stationsExits']['doors'];
+            if ($exit->load($data['stationsExits'], '') && $exit->validate()) {
+                if (!$exit->save()) {
+                    Yii::$app->response->statusCode = 400;
+                    return [
+                        'status' => 'error',
+                        'message' => 'Failed to save station exit',
+                        'errors' => $exit->errors,
+                    ];
+                }
+            } else {
+                Yii::$app->response->statusCode = 400;
+                return [
+                    'status' => 'error',
+                    'message' => 'Station exit validation failed',
+                    'errors' => $exit->errors,
+                ];
+            }
         }
     }
 }
